ae13e737fb0a3b80fe03cc8b4f00819861598da2
maksiurino <153639807+maksiurino@users.noreply.github.com>
2026-02-22 14:22:31 +0100

    Adding Features

---
+++ net/minecraft/Minecraft.cs
@@ -0,0 +1,9 @@
+﻿using Godot;
+using System;
+
+namespace MinecraftGodotEdition.net.minecraft;
+
+public partial class Minecraft : Node
+{
+    public static bool HasSeenSplashScreen = false;
+}
--- net/minecraft/client/gui/screens/TitleScreen.cs
+++ net/minecraft/client/gui/screens/TitleScreen.cs
@@ -5,18 +5,34 @@ namespace MinecraftGodotEdition.net.minecraft.client.gui.screens;
 
 public partial class TitleScreen : Screen
 {
+	private bool _hasCompletedAnimation;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		if (Minecraft.HasSeenSplashScreen)
+		{
+			var splashRenderer = GetNode<SplashRenderer>("SplashRenderer");
+			splashRenderer.QueueFree();
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var splashRenderer = GetNode<SplashRenderer>("SplashRenderer");
-		if (splashRenderer.HasCompletedAnimation)
+		SplashRenderer splashRenderer = null;
+		if (HasNode("SplashRenderer"))
 		{
-			GD.Print("Donenenenenenenne!!!");
+			splashRenderer = GetNode<SplashRenderer>("SplashRenderer");
+		}
+		if (splashRenderer != null)
+		{
+			if (splashRenderer.HasCompletedAnimation)
+			{
+				_hasCompletedAnimation = true;
+				splashRenderer.QueueFree();
+				Minecraft.HasSeenSplashScreen = true;
+			}
 		}
 	}
 
